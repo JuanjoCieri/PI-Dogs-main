95cbc7a34bc2580c97b4da8dd58026daee641850
JuanjoCieri <juin@hotmail.com.ar>
2022-07-31 06:12:52 -0300

    Deploy

--- client/src/redux/actions.js
+++ client/src/redux/actions.js
@@ -10,7 +10,7 @@ export const GET_DOG_BY_NAME = "GET_DOG_BY_NAME"
 export function getAllDogs() {
     return async function (dispatch) {
         try {
-            var json = await axios.get("http://localhost:3001/dogs")
+            var json = await axios.get("https://api-dogs-cieri.herokuapp.com/dogs")
             return dispatch({
                 type: "GET_ALL_DOGS",
                 payload: json.data
@@ -24,7 +24,7 @@ export function getAllDogs() {
 export function getAllTemperaments () {
     return async function (dispatch) {
         try {
-            var json = await axios.get("http://localhost:3001/temperaments")
+            var json = await axios.get("https://api-dogs-cieri.herokuapp.com/temperaments")
             return dispatch({
                 type: "GET_ALL_TEMPERAMENTS",
                 payload: json.data
@@ -38,7 +38,7 @@ export function getAllTemperaments () {
 export function getDog (id) {
     return async function (dispatch) {
         try {
-            var json = await axios.get(`http://localhost:3001/dog/${id}`)
+            var json = await axios.get(`https://api-dogs-cieri.herokuapp.com/dog/${id}`)
             return dispatch({
                 type: "GET_DOG",
                 payload: json.data
@@ -66,7 +66,7 @@ export function getDog (id) {
 export function createDog (payload) {
     return async function (dispatch) {
         try {
-            var json = await axios.post(`http://localhost:3001/dogs`,payload)
+            var json = await axios.post(`https://api-dogs-cieri.herokuapp.com/dogs`,payload)
             return dispatch({
                 type: "CREATE_DOG",
                 payload: json.data
@@ -86,7 +86,7 @@ export function clearDetails() {
 export function getDogByName (name) {
     return async function (dispatch) {
         try {
-            var json = await axios.get(`http://localhost:3001/dogs?name=${name}`)
+            var json = await axios.get(`https://api-dogs-cieri.herokuapp.com/dogs?name=${name}`)
             return dispatch({
                 type: "GET_DOG_BY_NAME",
                 payload: json.data
